20385812a69e485729250e19e24d6e9f88bfc5e6
ndri <nicolasdri92@gmail.com>
2024-04-15 13:24:41 -0300

    [feature/staging]
    fix configuración database global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,34 @@
-import { Module } from '@nestjs/common';
-import { RouterModule } from '@nestjs/core';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { RouterModule } from "@nestjs/core";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from "./modules/auth/auth.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: "mysql",
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USER"),
+        password: configService.get("DB_PASS"),
+        database: configService.get("DB_DATABASE"),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: configService.get("DB_SYNC").toLowerCase() == "true",
+      }),
+    }),
     AuthModule,
     RouterModule.register([
       {
-        path: 'auth',
+        path: "auth",
         module: AuthModule,
       },
     ]),
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,36 +1,16 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { DataSource } from "typeorm";
 
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { UserEntity } from './entities';
-import { ENCRYPT_SERVICE, REGISTER_FACTORY_SERVICE } from './interfaces';
-import { EncryptService, RegisterFactoryService } from './services';
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { UserEntity } from "./entities";
+import { ENCRYPT_SERVICE, REGISTER_FACTORY_SERVICE } from "./interfaces";
+import { EncryptService, RegisterFactoryService } from "./services";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASS'),
-        database: configService.get('DB_DATABASE'),
-        entities: [UserEntity],
-        synchronize: configService.get('DB_SYNC').toLowerCase() == 'true',
-      }),
-    }),
-    TypeOrmModule.forFeature([UserEntity]),
-  ],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [AuthController],
   providers: [
     AuthService,
