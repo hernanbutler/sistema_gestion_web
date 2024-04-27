ed3bbb6799b3f9db4e7bad77620a34b91682e900
ndri <nicolasdri92@gmail.com>
2024-04-27 15:37:11 -0300

    [Swagger] Completed

--- src/modules/activity/activity.controller.ts
+++ src/modules/activity/activity.controller.ts
@@ -8,6 +8,15 @@ import {
   Patch,
   UseGuards,
 } from "@nestjs/common";
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from "@nestjs/swagger";
 
 import { Roles } from "src/decorators";
 import { AuthGuard, RolesGuard } from "src/guards";
@@ -23,14 +32,23 @@ import {
 } from "./dtos";
 import { ActivityService } from "./activity.service";
 
+@ApiTags("Actividad")
 @Controller("activity")
 @UseGuards(AuthGuard)
+@ApiBearerAuth()
 export class ActivityController {
   constructor(private readonly activityService: ActivityService) {}
 
   @Post()
   @Roles(Rol.ADMINISTRADOR)
   @UseGuards(RolesGuard)
+  @ApiOperation({ summary: "Creación de actividad" })
+  @ApiCreatedResponse({
+    type: Promise<RsCreateActivityDto>,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Error al crear actividad",
+  })
   async create(
     @Body() rqCreateActivityDto: RqCreateActivityDto
   ): Promise<RsCreateActivityDto> {
@@ -38,16 +56,43 @@ export class ActivityController {
   }
 
   @Get(":id")
+  @ApiOperation({ summary: "Búsqueda de actividad" })
+  @ApiOkResponse({
+    type: Promise<RsGetActivityDto>,
+  })
+  @ApiNotFoundResponse({
+    description: "La actividad no existe",
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Error al obtener actividad",
+  })
   async findOne(@Param("id") id: string): Promise<RsGetActivityDto> {
     return await this.activityService.findOne(+id);
   }
 
   @Get()
+  @ApiOperation({ summary: "Búsqueda de actividades" })
+  @ApiOkResponse({
+    type: Promise<RsGetActivitiesDto>,
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Error al obtener actividad",
+  })
   async findAll(): Promise<RsGetActivitiesDto> {
     return await this.activityService.findAll();
   }
 
   @Patch(":id")
+  @ApiOperation({ summary: "Modificación de actividad" })
+  @ApiOkResponse({
+    type: Promise<RsUpdateActivityDto>,
+  })
+  @ApiNotFoundResponse({
+    description: "La actividad no existe",
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Error al obtener actividad",
+  })
   async update(
     @Param("id") id: string,
     @Body() rqUpdateActivityDto: RqUpdateActivityDto
@@ -58,6 +103,16 @@ export class ActivityController {
   @Delete(":id")
   @Roles(Rol.ADMINISTRADOR)
   @UseGuards(RolesGuard)
+  @ApiOperation({ summary: "Eliminación de actividad" })
+  @ApiOkResponse({
+    type: Promise<RsDeleteActivityDto>,
+  })
+  @ApiNotFoundResponse({
+    description: "La actividad no existe",
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Error al eliminar actividad",
+  })
   async remove(@Param("id") id: string): Promise<RsDeleteActivityDto> {
     return await this.activityService.remove(+id);
   }
